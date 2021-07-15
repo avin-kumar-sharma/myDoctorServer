61b65a1b62fd829be6a2b4ea09f9d4b6eeadadce
avin-kumar-sharma <avin.kumar.sharma@gmail.com>
2021-07-15 15:52:24 +0530

    nodejs sever

---
+++ people.js
@@ -0,0 +1,7 @@
+// const people = ['ritika','ritss','jawa'];
+// const ages = [20,56,7,8];
+// console.log(people);
+
+// module.exports = {
+//     people,ages
+// }
---
+++ test.js
@@ -0,0 +1,7 @@
+// const name= 'ritss';
+// console.log(name);
+// const greet =(name)=>{
+//     console.log(`hello,${name}`);
+// }
+// greet('mario');
+// greet('ritss');
